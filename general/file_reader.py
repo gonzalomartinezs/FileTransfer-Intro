d18c4909252bd83bb7c16aeaf84e4fbfca8fd308
Agustin <aguscambiano@gmail.com>
2021-10-10 16:26:20 -0300

    Created file reader

---
+++ general/file_reader.py
@@ -0,0 +1,19 @@
+class FileReader:
+    
+    #Inicializa el objeto que lee el archivo, en caso de que falle la apertura
+    #del archivo tira IOError
+    def __init__(self, filename):
+        self.file = open(filename, 'rb')
+
+    #Lee y retorna hasta bytes_to_read bytes del archivo que se recibio en el constructor
+    #Retorna un array de byteslike, en caso de que no quede nada por leer en el archivo
+    #tira la excepcion EOFError
+    def read_next_section(self, bytes_to_read):
+        bytes_read = self.file.read(bytes_to_read)
+        if bytes_read == b'':
+            raise EOFError()
+        return bytes_read
+
+    #Cierra el archivo
+    def close(self):
+        self.file.close()
