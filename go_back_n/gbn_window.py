a52dc5b45ffcce9908e660ed97b0e3ce85eecec7
Marcos Rolando <marcosrolando.mr@gmail.com>
2021-10-19 21:03:22 -0300

    Added functionality

--- go_back_n/gbn_window.py
+++ go_back_n/gbn_window.py
@@ -8,17 +8,14 @@ class GbnWindow:
         self.mutex = threading.Lock()
         self.window_size = window_size
         self.base = random.randrange(0, CONST_MAX_SEQ_NUM)
-        self.next_seq_num = self.window_base
+        self.next_seq_num = self.base
         self.packet_buffer = []
-
-    def is_full(self) -> bool:
-        self.mutex.acquire()
-        r = self.next_seq_num - self.base == self.window_size
-        self.mutex.release()
-        return r
+        self.cv = threading.Condition(self.mutex)
 
     def add_packet(self, packet: bytes):
         self.mutex.acquire()
+        while self._is_full():
+            self.cv.wait()
         #TODO: PROCESAR EL PAQUETE PARA QUE TENGA TODA LA METADATA
         self.packet_buffer.append(packet)
         self.next_seq_num += 1
@@ -31,24 +28,27 @@ class GbnWindow:
         self.packet_buffer.pop(0)
         self.mutex.release()
 
-    def get_size(self):
+    def get_size(self) -> int:
         self.mutex.acquire()
         r = self.window_size
         self.mutex.release()
         return r
 
-    def get_base(self):
+    def get_base(self) -> int:
         self.mutex.acquire()
         r = self.base
         self.mutex.release()
         return r
 
-    def get_next_seq_num(self):
+    def get_next_seq_num(self) -> int:
         self.mutex.acquire()
         r = self.next_seq_num
         self.mutex.release()
         return r
 
+
+    #Function description
+    #Returns True if there are not any messages whose acknowledge was not received, otherwise returns False
     def update_base(self, received_seq_number: int):
         self.mutex.acquire()
 
@@ -59,19 +59,28 @@ class GbnWindow:
             self.base = received_seq_number + 1
             if (self.base > CONST_MAX_SEQ_NUM): #This is only for the edge case where received_seq_number == CONST_MAX_SEQ_NUM
                 self.base = 0
-
+            self.cv.notify_all()
+        is_buffer_empty = len(self.packet_buffer) == 0
         self.mutex.release()
-    
 
+        return is_buffer_empty
+    
+    def get_unacknowledged_packets(self):
+        self.mutex.acquire()
+        ret_val = self.packet_buffer
+        self.mutex.release()
+        return ret_val
+        
     #PRIVATE
+    def _is_full(self) -> bool:
+        return (self.next_seq_num - self.base) == self.window_size
+
     def _get_packets_between(self, num):
         acknowledged_packets = 0
         aux = num - self.base
         if (aux < 0):
-            #TODO: CHEQUEAR SI ESTA CUENTA ESTA BIEN
             acknowledged_packets = ((CONST_MAX_SEQ_NUM + 1) - self.base) + (num + 1)
         else:
-            #TODO: CHEQUEAR SI ESTA CUENTA ESTA BIEN
             acknowledged_packets = aux + 1
 
         return acknowledged_packets
