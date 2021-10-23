e2f6a9ada387eb0596a02470510a936af23bdc60
Marcos Rolando <marcosrolando.mr@gmail.com>
2021-10-23 18:10:06 -0300

    ReliableUDPSocket now accepts multiple clients

--- general/realiable_udp_socket.py
+++ general/realiable_udp_socket.py
@@ -57,25 +57,15 @@ class ReliableUDPSocket:
         self._initialize_connection(r_addr, connection_seq_num)
 
 
-    def listen(self): #TODO, por ahora solo la uso para setear el accepted_connections
+    def listen(self, max_connections: int):
         self.accepted_connectons = AcceptedConnections()
+        self.keep_listening = True
+        self.new_connections_queue = Queue(max_connections)
+        self.thread = threading.Thread(target = self._listen_for_connections, daemon=True)
+        self.thread.start()
 
     def accept(self): #TODO indicar que devuelve un dato de tipo ReliableUDPSocket
-        connection_accepted = False
-
-        while not connection_accepted:
-            packet, addr = self.sckt.recvfrom(shared_constants.CONST_MAX_BUFFER_SIZE)
-            if (packet[0] == shared_constants.SYN_TYPE_NUM) and (not addr in self.accepted_connectons):
-                connection_seq_num = int.from_bytes(packet[1:3], byteorder='big', signed=False)
-                n_sckt = ReliableUDPSocket(self.use_goback_n)
-                n_sckt.accepted_connectons = self.accepted_connectons
-                n_sckt._initialize_connection(addr, connection_seq_num)
-                n_sckt.sender.send(shared_constants.OK_TYPE_NUM.to_bytes(1, byteorder='big', signed=False) + self.base_seq_num.to_bytes(2, byteorder='big', signed=False), add_metadata=False)
-                self.accepted_connectons.add_connection(addr)
-                connection_accepted = True
-
-        return n_sckt
-        
+        return self.new_connections_queue.get()
 
     def send(self, msg: bytes):
         self.sender.send(msg)
@@ -84,6 +74,7 @@ class ReliableUDPSocket:
         return self.receiver.receive()
 
     def close(self): #TODO borrar del AcceptedConnections el addr asignado si es que el socket fue creado por un accept
+        #TODO chequear si este socket es de tipo listen o no para setear la variable booleana para que corte y joinear con el thread
         self.sender.close()
         self.receiver.close()
         self.sckt.close()
@@ -104,6 +95,19 @@ class ReliableUDPSocket:
         self.thread = threading.Thread(target = self._receive_messages, daemon=True)
         self.thread.start()
 
+    def _listen_for_connections(self):
+        while self.keep_listening:
+            packet, addr = self.sckt.recvfrom(shared_constants.CONST_MAX_BUFFER_SIZE)
+            if self.new_connections_queue.full(): # We ignore the connection request if the queue is full
+                continue
+            if (packet[0] == shared_constants.SYN_TYPE_NUM) and (not addr in self.accepted_connectons):
+                connection_seq_num = int.from_bytes(packet[1:3], byteorder='big', signed=False)
+                n_sckt = ReliableUDPSocket(self.use_goback_n)
+                n_sckt.accepted_connectons = self.accepted_connectons
+                n_sckt._initialize_connection(addr, connection_seq_num)
+                n_sckt.sender.send(shared_constants.OK_TYPE_NUM.to_bytes(1, byteorder='big', signed=False) + self.base_seq_num.to_bytes(2, byteorder='big', signed=False), add_metadata=False)
+                self.accepted_connectons.add_connection(addr)
+                self.new_connections_queue.put(n_sckt)
 
     #TODO hay que chequear que los mensajes nos vengan del tipo con el que entablamos la conexion
     def _receive_messages(self): #TODO tenemos que chequear que los mensajes esten bien armados en cada caso, por ej que el mensaje de tipo ACK no tenga mas de 2 bytes despues del byte del ACK (que son los 2 bytes del seq_num)
